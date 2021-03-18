e8f24de96ceb6bf2b90e40989f5387af67a796de
scottstacey <scott.stacey@lincoln.ox.ac.uk>
2021-03-18 11:00:57 +0000

    Modified RhaSPID Model so degradation of X* releases a RhaS

--- Code/RhaS PID/RhaSPID.m
+++ Code/RhaS PID/RhaSPID.m
@@ -6,46 +6,48 @@ clear; clc; clf;
 % Integral control provided via sRNA annihilation.
 %% Initialising Global Parameters 
 
-global k_1
-global U
-global K_U
-global gamma_m
-global theta
-global k_2 
-global delta_1
-global alpha_1
-global alpha_2 
-global delta_2
-global k_3 
-global K_X
-global gamma_s
-global k_4
-global alpha_3
-global alpha_4
-global delta_3
-global delta_4
-global setpoint
+global k_1       % Maximal transcription rate of Z1
+global U         % Concentration of Inducer U in nM
+global K_U       % Concentraton of U at which Z1 transscription is at half maximal rate nM
+global gamma_m   % Degrgadation and dilution rate of mRNA 
+global theta     % Annihilation/binding rate of sRNA and mRNA 
+global k_2       % Translation rate of RhaS from Z1 mRNA 
+global delta_1   % Diltuion rate of RhaS based on E. coli doubling time of 30 minutes
+global alpha_1   % Dimerisation rate of RhaS
+global alpha_2   % Dissociation rate of RhaS dimer 
+global delta_2   % Dilution rate of RhaS Dimer
+global k_3       % Maximal rate of transcription of Z2 sRNA 
+global K_X       % Concentration of X (RhaS Dimer) at which Z2 is transcribed at half maximal rate nM
+global gamma_s   % Degradation and dilution rate of sRNA 
+global k_4       % Prodcution rate of the mutant RhaS (RhaS*)
+global alpha_3   % Dimmerisation rate between RhaS and RhaS*
+global alpha_4   % Dissociation rate of X* (RhaS-RhaS*)
+global deg       % Degradation rate due to a degradation tag
+global delta_3   % Degradation and Dilution rate of RhaS*
+global delta_4   % Degradation and Dilution rate of X* 
+global setpoint  % The setpoint concentration of the output X under idealised integral control. (When gammas=0)
 
 %% Parameter Values
 
 k_1      = 0.1 * 60 * 60;
 U        = 250000;
 K_U      = 178000; 
-gamma_m  = 0.0041*60*60*0;
+gamma_m  = 0.0041*60*60*0;  % set to zero for integral control
 theta    = 0.05 * 60 ;
 k_2      = 0.06 * 60 * 60;
 delta_1  = 0.00039*60*60;
 alpha_1  = 1 * 60 * 60;
 alpha_2  = 1 * 60 * 60;
-delta_2  = 0.00039*60*60;
+delta_2  = delta_1;
 k_3      = 1.5 * 60 * 60; 
 K_X      = 2600; 
-gamma_s  = 0.0008*60*60*0;
-k_4      = 3000;
+gamma_s  = 0.0008*60*60*0; % set to zero for integral control
+k_4      = 0;              % Set to zero for PI controller, otherwise non-zero
 alpha_3  = 1 * 60 * 60;
 alpha_4  = 1 * 60 * 60;
-delta_3  = 0.00039*60*60 + 0;
-delta_4  = 0.00039*60*60 + 0;
+deg      = 0;             % Degradation rate due to a degradation tag
+delta_3  = delta_1 + deg; % Dilution Rate + Degradation Rate 
+delta_4  = delta_1 + deg; % Dilution Rate + Degrdation Rate 
 setpoint = (k_1 * K_X * U) / ((k_3 * K_U) + (k_3 * U) - (k_1 * U)); 
 
 %% State is [Z1 Z2 RhaS X RhaS* X*]
@@ -87,8 +89,10 @@ global K_X
 global k_4
 global alpha_3
 global alpha_4
+global deg
 global delta_3
 global delta_4
+
 
 Z_1       = S(1);
 Z_2       = S(2);
@@ -99,7 +103,7 @@ X_star    = S(6);
 
 dZ_1dt       = ((k_1 * U)/(K_U + U)) - (gamma_m * Z_1) - (theta * Z_1 * Z_2);
 dZ_2dt       = ((k_3 * X)/(K_X + X)) - (gamma_s * Z_2) - (theta * Z_1 * Z_2);
-dRhaSdt      = (k_2 * Z_1) - (delta_1 * RhaS) - (2 * alpha_1 * RhaS * RhaS) + (2 * alpha_2 * X) - (alpha_3 * RhaS * RhaS_star) + (alpha_4 * X_star);
+dRhaSdt      = (k_2 * Z_1) - (delta_1 * RhaS) - (2 * alpha_1 * RhaS * RhaS) + (2 * alpha_2 * X) - (alpha_3 * RhaS * RhaS_star) + (alpha_4 * X_star) + (deg * X_star);
 dXdt         = (alpha_1 * RhaS * RhaS) - (alpha_2 * X) - (delta_2 * X);
 dRhaS_stardt = k_4 - (delta_3 * RhaS_star) - (alpha_3 * RhaS * RhaS_star) + (alpha_4 * X_star);
 dX_stardt    = (alpha_3 * RhaS * RhaS_star) - (alpha_4 * X_star) - (delta_4 * X_star);
